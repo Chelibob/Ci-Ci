c5fdf54c5c020946a4044094f3714a3e89630d4c
Chelibob <nicekolya.a7x@gmail.com>
2022-04-21 22:38:08 +0500

    Упражнение 2.10 complete
    Напишите функцию lower, которая переводит большие буквы в малые, используя
    условное выражение (а не конструкцию if-else).

--- program.c
+++ program.c
@@ -6,37 +6,18 @@
 
 
 
-unsigned setbits(unsigned x, int p, int n, int y)
-{
-    return (~((~(~0<<n))<<p)&x)|((~(~0<<n)&y)<<p);
-    
-}
 
 
-unsigned invert(unsigned x, int p,char n){
-    return (~x&(~(~0<<n)<<p)) | (x & ~(~(~0<<n)<<p));
+void lower(char s[]){
+    for(int i = 0; s[i] != '\0'; i++)
+        (isupper(s[i])) ? s[i] = tolower(s[i]): s[i];
 }
 
-int binary_search(char s[], size_t arrsize, char item){
-    int low = 0;
-    int high = arrsize;
-    while(low <= high){
-        int mid = (low+high)/2;
-        int guess = s[mid];
-        if(guess == item)
-            return mid;
-        if(guess > item)
-            high = mid - 1;
-        else
-            low = mid + 1;
-    }
-    return -1;
-}
 
 int main(){
-    char s[] = "13579";
-    size_t arrsize = sizeof(s)/sizeof(s[0]);
-    printf("%d\n", binary_search(s,arrsize,'3'));
+    char s1[] = "HeLLo World";
+    lower(s1);
+    printf("%s\n",s1);
     return 0;
 }
 
