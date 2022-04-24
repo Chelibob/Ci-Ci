a7a4e89a90a5b0efc7783e4e99beca4bf139c26e
Chelibob <nicekolya.a7x@gmail.com>
2022-04-24 17:51:14 +0500

    Упражнение 3.2 complete. Напишите функцию escape(s, t), которая при копировании текста из t в s преобразует такие символы, как новая строка и табуляция в "видимые последовательности символов" (вроде \n и \t).
    Обратная функция тоже на месте.

--- program.c
+++ program.c
@@ -4,20 +4,68 @@
 #include <float.h>
 #include <ctype.h>
 
+#define ARRSIZE 1000
 
 
 
-
-void lower(char s[]){
-    for(int i = 0; s[i] != '\0'; i++)
-        (isupper(s[i])) ? s[i] = tolower(s[i]): s[i];
+void escape(char t[],char s[]){
+    int i,j;
+    for(j = i = 0; t[i] != '\0'; i++,j++){
+        switch (t[i])
+        {
+        case '\n':
+            s[j++] = '\\';
+            s[j] = 'n';
+            break;
+        case '\t':
+            s[j++] = '\\';
+            s[j] = 't';
+            break;
+        default:
+            s[j] = t[i];
+            break;
+        }
+    }
+    s[j] = '\0';
 }
 
 
+void unescape(char t[],char s[]){
+    int i,j;
+    for(i = j = 0; t[i] != '\0'; i++, j++){
+        switch (t[i] + t[i+1]){
+        case '\\' + 'n':
+            s[j] = '\n';
+            i++;
+            break;
+        case '\\' + 't':
+            s[j] = '\t';
+            i++;
+            break;
+        default:
+            s[j] = t[i];
+            break;
+        }
+    }
+    s[j] = '\0';
+}
+
+
+
+
 int main(){
-    char s1[] = "HeLLo World";
-    lower(s1);
-    printf("%s\n",s1);
+    int c, i;
+    char s1[ARRSIZE];
+    char s2[ARRSIZE];
+    char s3[ARRSIZE];
+    for(i = 0; (c = getchar()) != EOF; i++)
+        s1[i] = c;
+    s1[i] = '\0';
+    printf("%s\n", s1);
+    escape(s1,s2);
+    printf("%s\n", s2);
+    unescape(s2,s3);
+    printf("%s\n", s3);
     return 0;
 }
 
